6c0df90cd1cbba5eeb576b3b85748049cee6e774
HenryAtkinson <48465549+HenryAtkinson@users.noreply.github.com>
2019-03-12 19:45:50 -0600

    Update view_items_inorder.php

--- chef/view_items_inorder.php
+++ chef/view_items_inorder.php
@@ -30,12 +30,14 @@ if (isset($_POST["item_id"]))
 {
 	$item_id = $_POST["item_id"];
 	$query = "UPDATE ordered_items SET item_complete = true WHERE order_id =" . $order_id . " AND item_id = " . $item_id;
+	echo $query;
 	$insert_order_complete = $conn->query($query);
 	if (!$insert_order_complete) 
 	{
 		echo "INSERT failed: $query<br>" . $conn->error . "<br><br>";
 	}
 	$query = "SELECT item_complete FROM ordered_items WHERE item_compelte = false AND order_id = " . $order_id;
+	echo $query;
 	$item_compelte_results = $conn->query($query);
 	$count = $item_compelte_results->num_rows;
 	if (count == 0)
