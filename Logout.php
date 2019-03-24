ff5ad61c4207bd0479cebd19770525b82147e886
DevanBertoia <47012984+DevanBertoia@users.noreply.github.com>
2019-03-24 08:58:27 -0600

    Uploaded new index and logout file
    I forgot to do this yesterday so sorry if you tried to pull and it wasn't working. *facepalm

---
+++ Logout.php
@@ -0,0 +1,11 @@
+<?php 
+session_start();
+// delete all of the session variables
+$_SESSION = array();
+session_destroy();
+	
+// redirect the user back to the login page
+header("Location: index.html");
+exit();
+
+?>
